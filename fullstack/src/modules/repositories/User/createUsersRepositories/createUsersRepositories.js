d82f61b3f90a973e72329106f619cc93db003fca
Victor Ramon <victorrgds02@gmail.com>
2022-08-09 20:10:45 -0300

    fix: create user route was not returning response properly

--- fullstack/src/modules/repositories/User/createUsersRepositories/createUsersRepositories.js
+++ fullstack/src/modules/repositories/User/createUsersRepositories/createUsersRepositories.js
@@ -1,40 +1,32 @@
 const {
-    getTransaction,
-    commitTransaction,
-    rollbackTransaction
-} = require('../../../common/handlers')
+  getTransaction,
+  commitTransaction,
+  rollbackTransaction,
+} = require("../../../common/handlers");
 
+const createUserRepositories = async ({ user } = {}) => {
+  const { transaction } = await getTransaction();
 
-const createUserRepositories = async ({
-    user
-} = {}) => {
-    const { transaction } = await getTransaction();
+  try {
+    const user_created = await transaction("users").insert(user);
 
-    try {
-        const {
-            user_created
-        } = await transaction('users').insert(user)
-        
-        const has_response = Array.isArray(user_created) && user_created.length > 0;
+    const has_response = Array.isArray(user_created) && user_created.length > 0;
 
-        if (!has_response) {
-            return {
-                user_created: []
-            }
-        }
-
-        await commitTransaction({transaction})
-
-        return {
-            user_created
-        }
-
-    } catch (err) {
-        rollbackTransaction({transaction})
-        throw new Error(err)
+    if (!has_response) {
+      return {
+        user_created: [],
+      };
     }
-}
+
+    await commitTransaction({ transaction });
+
+    return { user_created };
+  } catch (err) {
+    rollbackTransaction({ transaction });
+    throw new Error(err);
+  }
+};
 
 module.exports = {
-    createUserRepositories
-}
+  createUserRepositories,
+};
