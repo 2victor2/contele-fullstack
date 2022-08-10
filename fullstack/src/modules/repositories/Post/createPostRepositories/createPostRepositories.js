e59b6b90418b2c9b0bb0f44d4189fb9006624134
Victor Ramon <victorrgds02@gmail.com>
2022-08-10 09:43:47 -0300

    fix: create post route was not returning response properly

--- fullstack/src/modules/repositories/Post/createPostRepositories/createPostRepositories.js
+++ fullstack/src/modules/repositories/Post/createPostRepositories/createPostRepositories.js
@@ -1,37 +1,32 @@
 const {
-    getTransaction,
-    commitTransaction,
-    rollbackTransaction
-} = require('../../../common/handlers')
+  getTransaction,
+  commitTransaction,
+  rollbackTransaction,
+} = require("../../../common/handlers");
 
+const createPostRepositories = async ({ post } = {}) => {
+  const { transaction } = await getTransaction();
 
-const createPostRepositories = async ({
-    post
-} = {}) => {
-    const { transaction } = await getTransaction();
+  try {
+    const post_created = await transaction("posts").insert(post);
 
-    try {
-        const post_created = await transaction('posts').insert(post)
-        
-        const has_response = !Array.isArray(post_created) && post_created.length > 0;
+    const has_response = Array.isArray(post_created) && post_created.length > 0;
 
-        if (!has_response) {
-            return {
-                post_created: []
-            }
-        }
-
-
-        return {
-            post_created
-        }
-
-    } catch (err) {
-        rollbackTransaction({transaction})
-        throw new Error(err)
+    if (!has_response) {
+      return {
+        post_created: [],
+      };
     }
-}
+
+    return {
+      post_created,
+    };
+  } catch (err) {
+    rollbackTransaction({ transaction });
+    throw new Error(err);
+  }
+};
 
 module.exports = {
-    createPostRepositories
-}
+  createPostRepositories,
+};
