6a63323a8c9d581eac908353cdce4627e67158b6
Victor Ramon <victorrgds02@gmail.com>
2022-08-10 10:25:57 -0300

    fix: list post by user id was always returning empty array

--- fullstack/src/modules/services/Post/listPostService/listPostService.js
+++ fullstack/src/modules/services/Post/listPostService/listPostService.js
@@ -1,33 +1,28 @@
-const { getUserByIdService } = require("../../User/getUserByIdService/getUserByIdService");
+const {
+  getUserByIdService,
+} = require("../../User/getUserByIdService/getUserByIdService");
 const { getPostByUserIdRepositories } = require("../../../repositories");
 
-const getPostByUserIdService = async ({
-    user_id
-}) => {
+const getPostByUserIdService = async ({ user_id }) => {
+  const { user } = await getUserByIdService({
+    user_id,
+  });
+  
+  const has_author = Array.isArray(user) && user.length > 0;
 
-    const {
-        user
-    } = await getUserByIdService({
-        user_id
-    })
+  if (has_author === false) {
+    throw new Error("Missing author in database");
+  }
 
-    const has_author = Array.isArray(user) && user.length > 0;
+  const { posts = [] } = await getPostByUserIdRepositories({
+    user_id,
+  });
 
-    if(has_author === false) {
-        throw new Error("Missing author in database")
-    }
-
-    const {
-        posts = []
-    } = await getPostByUserIdRepositories({
-        user_id
-    });
-
-    return {
-        posts
-    };
-}
+  return {
+    posts,
+  };
+};
 
 module.exports = {
-    getPostByUserIdService
-}
+  getPostByUserIdService,
+};
